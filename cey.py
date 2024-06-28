8bfb3756d40d90a413065a7eb11ccc619e47fd0e
ceysd <135987170+ceysd@users.noreply.github.com>
2024-06-29 00:25:26 +0300

    Update cey.py

--- cey.py
+++ cey.py
@@ -1,18 +1,27 @@
+import time
+import math
 def toplama(x,y):
     return x + y
 def çıkarma(x,y):
     return x - y
 def bölme(x,y):
-    if y != 0:
+    if y == 0:
+        print("Bir sayıyı 0'a bölemezsiniz!")
+    else:
         return x / y
 def çarpma(x,y):
     return x * y
 def karesinialma(x):
     return x ** 2
 def karekökünüalma(x):
-    return x ** 0.5
+    if x < 0:
+        print("Negatif sayıların karekökü alınamaz!")
+    else:
+        return x ** 0.5
 def yüzdehesaplama(x,y):
     return (x * y) / 100
+
+
 ekran = """
 (1) toplama
 (2) çıkarma
@@ -25,80 +34,109 @@ ekran = """
 (9) hesap makinesini sıfırla
 (0) çıkış"""
 
-son_işlem = None
+son_işlem = 0
 
-try:
+print("\n*************************************** UYARI *************************************** \n")
+print("\n  *** Son işlem üzerinden hesaplama yapmak isterseniz, ikinci sayiyi girmeden enter tuşuna basabilirsiniz! *** \n ")
+print("\n******************************************************************************\n")
 
-    while True:
-
-        seçim = int(input("Yapmak istediğiniz işlem numarasını giriniz(1-9):                    Programdan çıkmak için 0'ı tuşlayınız."))
+while True:
+    try:
+        
+        print(ekran)
+       
+        seçim = int(input("Yapmak istediğiniz işlem numarasını giriniz(1-9) (Programdan çıkmak için 0'ı tuşlayınız.) : "))
 
         if seçim == 1:
-            soru1 = float(input("Toplama işlemi için ilk sayıyı giriniz:"))
-            soru2 = float(input("Toplama işlemi için ikinci sayıyı giriniz:"))
-            sonuç = toplama(soru1,soru2)
-            print("Sonuç:", sonuç)
-            son_işlem = sonuç
-        elif seçim == 2:
-            soru3 = float(input("Çıkarma işlemi için ilk sayıyı giriniz:"))
-            soru4 = float(input("Çıkarma işlemi için ikinci sayıyı giriniz:"))
-            sonuç = çıkarma(soru3,soru4)
-            print("Sonuç:", sonuç)
-            son_işlem = sonuç
-        elif seçim == 3:
-            soru5 = float(input("Bölme işlemi işlemi ilk sayıyı giriniz:"))
-            soru6 = float(input("Bölme işlemi için ikinci sayıyı giriniz:"))
-            if soru6 == 0:
-                print("Bir sayıyı 0'a bölemezsiniz.")
-                break
+            sayı1 = input("Toplama işlemi için ilk sayıyı giriniz:")
+            sayı2 = input("Toplama işlemi için ikinci sayıyı giriniz:")
+
+            if (sayı2.isnumeric()):
+                sonuç = toplama(float(sayı1),float(sayı2))
+                son_işlem = sonuç
             else:
-                sonuç = bölme(soru5,soru6)
-            print("Sonuç:", sonuç)
-            son_işlem = sonuç
+                son_işlem += float(sayı1)
+
+
+        elif seçim == 2:
+            sayı3 = input("Çıkarma işlemi için ilk sayıyı giriniz:")
+            sayı4 = input("Çıkarma işlemi için ikinci sayıyı giriniz:")
+
+            if (sayı4.isnumeric()):
+                sonuç = çıkarma(float(sayı3),float(sayı4))
+                son_işlem = sonuç
+            else:
+                son_işlem -= float(sayı3)
+
+        elif seçim == 3:
+            sayı5 = input("Bölme işlemi işlemi ilk sayıyı giriniz:")
+            sayı6 = input("Bölme işlemi için ikinci sayıyı giriniz:")
+
+            if (sayı6.isnumeric()):
+                
+                if float(sayı6) == 0:
+                    print("Bir sayıyı 0'a bölemezsiniz.")
+                    continue
+                else:
+                    sonuç = bölme(float(sayı5),float(sayı6))
+            else:
+                son_işlem /= float(sayı5)
+        
+            
         elif seçim == 4:
-            soru7 = float(input("Çarpma işlemi için ilk sayıyı giriniz:"))
-            soru8 = float(input("Çarpma işlemi için ikinci sayıyı giriniz:"))
-            sonuç = çarpma(soru7,soru8)
-            print("Sonuç:", sonuç)
-            son_işlem = sonuç
+            sayı7 = input("Çarpma işlemi için ilk sayıyı giriniz:")
+            sayı8 = input("Çarpma işlemi için ikinci sayıyı giriniz:")
+
+            if (sayı8.isnumeric()):
+                sonuç = çarpma(float(sayı7),float(sayı8))
+                son_işlem = sonuç
+
+            else:
+                son_işlem *= float(sayı7)
+
         elif seçim == 5:
-            soru9 = int(input("Karesini almak istediğiniz sayıyı giriniz:"))
-            sonuç = karesinialma(soru9)
-            print("Sonuç:", sonuç)
+            sayı9 = int(input("Karesini almak istediğiniz sayıyı giriniz:"))
+            sonuç = karesinialma(sayı9)
             son_işlem = sonuç
+
         elif seçim == 6:
-            soru10 = float(input("Karekökünü hesaplamak istediğiniz sayıyı giriniz:"))
-            if soru10 < 0:
+            sayı10 = float(input("Karekökünü hesaplamak istediğiniz sayıyı giriniz:"))
+            if sayı10 < 0:
                 print("Negatif sayıların karekökü alınmaz.")
-            elif soru10 == 0:
+            elif sayı10 == 0:
                 print("0'ın karekökü alınmaz.")
             else:
-                sonuç = karekökünüalma(soru10)
-                print("Sonuç:", sonuç)
+                sonuç = karekökünüalma(sayı10)
             son_işlem = sonuç
+
         elif seçim == 7:
-            soru11 = float(input("Yüzdesini hesaplatmak istediğiniz sayıyı giriniz:"))
-            soru12 = float(input("Seçtiğiniz sayının yüzde kaçını hesaplatmak istiyorsunuz:"))
-            sonuç = yüzdehesaplama(soru11,soru12)
-            print("Sonuç:", sonuç)
+            sayı11 = float(input("Yüzdesini hesaplatmak istediğiniz sayıyı giriniz:"))
+            sayı12 = float(input("Seçtiğiniz sayının yüzde kaçını hesaplatmak istiyorsunuz:"))
+            sonuç = yüzdehesaplama(sayı11,sayı12)
             son_işlem = sonuç
+            
         elif seçim == 8:
-            if son_işlem is not None:
-                print("Son yapılan işlem:", son_işlem)
+            if son_işlem == 0:
+                print("Hafıza boş")
             else:
-                print("Hafıza boş.")
+                print(son_işlem)
+            
+            
         elif seçim == 9:
-            son_işlem = None
+            son_işlem = 0
             print("Hafıza sıfırlandı.")
+            
         elif seçim == 0:
-            print("Sistemden çıkış yapıldı.")
+            print("Sistemden çıkış yapılıyor...")
+            time.sleep(1)
+            print( "\nSistemden çıkış yapıldı.")
             break
+    
+        else:
+            print("Hata olustu lütfen tekrar deneyiniz.")
 
-    else:
-        print("Hata olustu lütfen tekrar deneyiniz.")
+        print("\n **** \n İşlem Sonucu:", son_işlem, "\n ****")
 
-except ValueError:
-    print("Lütfen bir sayı değeri giriniz.")
-except ZeroDivisionError:
-    print("Bir sayıyı 0'a bölemezsiniz.")
-
+    except ValueError:
+        print("Bilinmeyen karakter girildi! Lütfen girdiğiniz sayıları kontrol edin! ")
+    
